f0e36e31e96876c8f5e9a249e5f586035b316ece
James McNee <james@jamesmcnee.co.uk>
2023-09-02 14:22:25 +0100

    Add alternative entrypoint
    - Expose a `persevereFor` entrypoint as an alternative to `persevere`.

--- lib/persevere.ts
+++ lib/persevere.ts
@@ -41,3 +41,13 @@ export class Persevere {
 export function persevere(): Persevere {
     return new Persevere();
 }
+
+/**
+ * Entrypoint into the persevere chain
+ *
+ * Follow up with a temporal binding method (e.g. {@link Persevere(atMost:instance)}) in order to continue building...
+ * @returns Persevere an instance of the {@link Persevere} class
+ */
+export function persevereFor(): Persevere {
+    return new Persevere();
+}
